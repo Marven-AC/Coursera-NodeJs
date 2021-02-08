bcbb9451dd9640f2c2a00d6daee083f7e9cfdf0b
Marven-AC <marven901@gmail.com>
2021-02-08 19:20:28 +0200

    CallBack Hell and Promises

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -7,54 +7,64 @@ const dboper = require('./operations');
 const url = 'mongodb://localhost:27017/';
 const dbname = 'confusion';
 
-MongoClient.connect(url, (err, client) => {
-    // assert to assure there is no error, checks error == null
-    assert.strictEqual(err, null);
+MongoClient.connect(url).then((client) => {
+    // assert to assure there is no error, checks error == null assert.strictEqual(err, null)
+
     console.log('Connected correctly to server');
     // connect to the db named confusion
     const db = client.db(dbname);
 
-    dboper.insertDocument(db, { name: "Vadonut", description: "Test" }, 'dishes', (result) => {
-        console.log('Insert Document:\n', result.ops);
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test" }, 'dishes')
+        .then((result) => {
+            console.log('Insert Document:\n', result.ops);
 
-        dboper.findDocuments(db, 'dishes', (docs) => {
+            return dboper.findDocuments(db, 'dishes');
+        })
+        .then((docs) => {
             console.log("Found documents:\n", docs);
 
-            dboper.updateDocument(db, { name: 'Vadonut' }, { description: "Updated Test" }, 'dishes', (result) => {
-                console.log("Updated Documents:\n", result.result);
+            return dboper.updateDocument(db, { name: 'Vadonut' }, { description: "Updated Test" }, 'dishes');
+        })
+        .then((result) => {
+            console.log("Updated Documents:\n", result.result);
 
-                dboper.findDocuments(db, 'dishes', (docs) => {
-                    console.log("Found documents:\n", docs);
+            return dboper.findDocuments(db, 'dishes');
+        })
+        .then((docs) => {
+            console.log("Found documents:\n", docs);
 
-                    db.dropCollection('dishes', (result) => {
-                        console.log("Droped collection:", result);
-                        client.close();
-                    });
-                });
-            });
-        });
-    });
+            return db.dropCollection('dishes');
+        })
+        .then((result) => {
+            console.log("Droped collection:", result);
+            return client.close();
+        })
+        .catch((err) => console.log(err));
+})
+    .catch((err) => console.log(err));
 
-    /*
-    const collection = db.collection('dishes');
 
-    collection.insertOne({ "name": "Uthappizza", "descripiton": "test" }, (err, result) => {
+
+
+/*
+const collection = db.collection('dishes');
+
+collection.insertOne({ "name": "Uthappizza", "descripiton": "test" }, (err, result) => {
+    assert.strictEqual(err, null);
+
+    console.log("After Insert:\n");
+    console.log(result.ops); // .ops how many operation carried out successfully
+
+    collection.find({}).toArray((err, docs) => {
         assert.strictEqual(err, null);
+        console.log('Found: \n');
+        console.log(docs); // docs all the document of the collection that match the criteria in this case all the documents will be returned
 
-        console.log("After Insert:\n");
-        console.log(result.ops); // .ops how many operation carried out successfully
-
-        collection.find({}).toArray((err, docs) => {
+        db.dropCollection('dishes', (err, result) => {
             assert.strictEqual(err, null);
-            console.log('Found: \n');
-            console.log(docs); // docs all the document of the collection that match the criteria in this case all the documents will be returned
 
-            db.dropCollection('dishes', (err, result) => {
-                assert.strictEqual(err, null);
-
-                client.close();
-            });
+            client.close();
         });
     });
-    */
 });
+*/
